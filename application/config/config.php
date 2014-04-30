c00f77cac81d0e7f710fa89683683241f874a62c
Invento <info@invento-dev.com>
2014-04-29 22:12:48 -0600

    Config from Server

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/prj1/tgss/';
+$config['base_url']	= 'http://tgss.tgssprinting.com/';
 
 /*
 |--------------------------------------------------------------------------
