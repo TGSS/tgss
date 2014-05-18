cdae086367b0608c5d575fa11f40d3ef9a7377ef
Wai Phyo <wpst006@gmail.com>
2014-05-18 16:15:30 +0630

    WPST

--- application/views/visitingcards/visitingcard-gallery.php
+++ application/views/visitingcards/visitingcard-gallery.php
@@ -4,34 +4,34 @@
 <table>
     <tr>
         <td>
-            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/C&K (JuJu).jpg" title="Chrysanthemum">
+            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/C&K (JuJu).jpg" title="C&K (JuJu)">
                 <img src="<?php echo base_url(); ?>media/visitingcards/thumbs/C&K (JuJu).jpg"/>
             </a>
         </td>
         <td>
-            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/MYT (Ju Ju).jpg" title="Desert">
+            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/MYT (Ju Ju).jpg" title="MYT (Ju Ju)">
                 <img src="<?php echo base_url(); ?>media/visitingcards/thumbs/MYT (Ju Ju).jpg"/>
             </a>
         </td>
         <td>
-            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/NG (Ju Ju).jpg" title="Hydrangeas">
+            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/NG (Ju Ju).jpg" title="NG (Ju Ju)">
                 <img src="<?php echo base_url(); ?>media/visitingcards/thumbs/NG (Ju Ju).jpg"/>
             </a>
         </td>
     </tr>
     <tr>
         <td>
-            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/PK (Ju Ju).jpg" title="Jellyfish">
+            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/PK (Ju Ju).jpg" title="PK (Ju Ju)">
                 <img src="<?php echo base_url(); ?>media/visitingcards/thumbs/PK (Ju Ju).jpg"/>
             </a>
         </td>
         <td>
-            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/RV (Ju Ju).jpg" title="Koala">
+            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/RV (Ju Ju).jpg" title="RV (Ju Ju)">
                 <img src="<?php echo base_url(); ?>media/visitingcards/thumbs/RV (Ju Ju).jpg"/>
             </a>
         </td>
         <td>
-            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/SB (Ju Ju).jpg" title="Lighthouse">
+            <a class="group1" href="<?php echo base_url(); ?>media/visitingcards/SB (Ju Ju).jpg" title="SB (Ju Ju)">
                 <img src="<?php echo base_url(); ?>media/visitingcards/thumbs/SB (Ju Ju).jpg"/>
             </a>
         </td>
