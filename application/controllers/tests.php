cfb4419468c1526bb18a564dc447dfd5715ef1d0
Ko Zin Thet <kozinthet@global-connect.asia>
2014-06-17 13:54:05 +0630

    update

--- application/controllers/tests.php
+++ application/controllers/tests.php
@@ -4,7 +4,7 @@ class Tests extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->library('PaypalRest');
+        //$this->load->library('PaypalRest');
     }
 
     public function test01() {
@@ -17,6 +17,11 @@ class Tests extends CI_Controller {
     public function test02() {
         redirect('https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-22J87495L70228255');
     }
+	
+	public function upload()
+	{
+		$this->load->view('test/upload');
+	}
 
 }
 
