822a6bfac1aa9ba9ecfa9616d3342e8e26758ce0
Wai Phyo <wpst006@gmail.com>
2014-07-21 09:28:42 +0630

    WPST

--- application/controllers/vouchers.php
+++ application/controllers/vouchers.php
@@ -3,42 +3,47 @@
 class Vouchers extends CI_Controller {
 
     public function __construct() {
-        parent::__construct();        
+        parent::__construct();
         $this->load->model('voucher_types_model');
         $this->load->model('vouchers_model');
     }
-   
-    public function index(){
-        $data['voucher_types']=$this->voucher_types_model->getVoucherTypes();
-        
+
+    public function index() {
+        $data['voucher_types'] = $this->voucher_types_model->getVoucherTypes();
+
         $data['template'] = "vouchers/index";
         $this->load->view('template', $data);
     }
-    
-    public function select($type_id){
+
+    public function select() {
         $this->load->helper('form');
-        
-        $data['parent_type_id']=$type_id;
-        $data['voucher_type']=$this->voucher_types_model->getVoucherTypes($type_id);
-        $data['card_type']=$this->vouchers_model->getCardType($type_id);
-        $data['quantity']=$this->vouchers_model->getQuantity($type_id);
-        
+
+        $data['voucher_type'] = $this->voucher_types_model->getVoucherTypes();
+
         $data['template'] = "vouchers/select";
         $this->load->view('template', $data);
     }
-    
-    public function get_total_price($parent_type_id,$card_type,$quantity){
-        
-        $data=$this->vouchers_model->getPrice($parent_type_id,$card_type,$quantity);
-        $total=$data[0]['price']*$quantity;
-        
-        $result['card_id']=$data[0]['card_id'];
-        $result['price']=$data[0]['price'];
-        $result['total']=$total;
-        
+
+    public function get_total_price($parent_type_id, $card_type, $quantity) {
+
+        $data = $this->vouchers_model->getPrice($parent_type_id, $card_type, $quantity);
+        $total = $data[0]['price'] * $quantity;
+
+        $result['card_id'] = $data[0]['card_id'];
+        $result['price'] = $data[0]['price'];
+        $result['total'] = $total;
+
         return $this->ajax_handler->responseSuccess($result);
     }
-       
+
+    public function get_voucher_type_data($voucher_type_id) {
+        $data['voucher_data'] = $this->voucher_types_model->getVoucherTypes($voucher_type_id);
+        $data['card_type'] = $this->vouchers_model->getCardType($voucher_type_id);
+        $data['quantity'] = $this->vouchers_model->getQuantity($voucher_type_id);
+        
+        return $this->ajax_handler->responseSuccess($data);
+    }
+
 }
 
 ?>
--- application/views/vouchers/voucher-form.php
+++ application/views/vouchers/voucher-form.php
@@ -1,47 +1,59 @@
+<style type="text/css">
+    #card_type_chosen .chosen-results {
+        height: 120px !important;
+        overflow-y: auto;
+    }
+</style>
+
 <?php
 $attributes = array('class' => 'form-horizontal', 'id' => 'vouchers', 'name' => 'vouchers');
 echo form_open('cards/vouchers', $attributes);
 ?>
 
 <div class="form-group">
-    <label class="col-sm-4 control-label">Size :</label>
-    <div class="col-sm-8">
-        <p class="form-control-static" id="size"><?php echo $voucher_type['size']; ?></p>
-    </div>
-</div>
-
-<div class="form-group">
-    <label class="col-sm-4 control-label">Quantity :</label>
-    <div class="col-sm-8">
-        <p class="form-control-static" id="type_quantity"><?php echo $voucher_type['type_quantity']; ?></p>
-    </div>
-</div>
-
-<div class="form-group">
-    <label class="col-sm-4 control-label">Printing :</label>
-    <div class="col-sm-8">
-        <p class="form-control-static" id="printing"><?php echo $voucher_type['printing']; ?></p>
-    </div>
-</div>
-
-<div class="form-group">
     <div class="col-sm-4 control-label">Card Type :</div>
     <div class="col-sm-8">
-        <select id="card_type" name="card_type" class="form-controls calculation-item chosen-select">
-            <?php foreach ($card_type as $key => $value) { ?>
-                <option value="<?php echo $value['card_type']; ?>"><?php echo $value['card_type_display']; ?></option>
+        <select id="voucher_type_id" name="voucher_type_id" class="form-controls chosen-select">
+            <?php foreach ($voucher_type as $key => $value) { ?>
+                <option value="<?php echo $value['type_id']; ?>"><?php echo $value['card_type_display']; ?></option>
             <?php } ?>
         </select>    
     </div>
 </div>
 
 <div class="form-group">
+    <label class="col-sm-4 control-label">Size :</label>
+    <div class="col-sm-8">
+        <p class="form-control-static" id="size"></p>
+    </div>
+</div>
+
+<div class="form-group">
+    <label class="col-sm-4 control-label">Quantity :</label>
+    <div class="col-sm-8">
+        <p class="form-control-static" id="type_quantity"></p>
+    </div>
+</div>
+
+<div class="form-group">
+    <label class="col-sm-4 control-label">Printing :</label>
+    <div class="col-sm-8">
+        <p class="form-control-static" id="printing"></p>
+    </div>
+</div>
+
+<div class="form-group calculation-item-container">
+    <div class="col-sm-4 control-label">Card Type :</div>
+    <div class="col-sm-8">
+        <select id="card_type" name="card_type" class="form-controls calculation-item chosen-select">            
+        </select>    
+    </div>
+</div>
+
+<div class="form-group calculation-item-container">
     <div class="col-sm-4 control-label">Quantity :</div>
     <div class="col-sm-8">
         <select id="quantity" name="quantity" class="form-controls calculation-item chosen-select">
-            <?php foreach ($quantity as $key => $value) { ?>
-                <option value="<?php echo $value['quantity']; ?>"><?php echo $value['quantity']; ?></option>
-            <?php } ?>
         </select> 
     </div>
 </div>
@@ -55,7 +67,6 @@ echo form_open('cards/vouchers', $attributes);
 
 <input type="hidden" id="card_id" name="card_id" class="controls" value=""/>
 <input type="hidden" id="card_type_hidden" name="card_type_hidden" class="controls" value=""/>
-<input type="hidden" id="parent_type_id_hidden" name="parent_type_id_hidden" class="controls" value="<?php echo $parent_type_id; ?>"/>
 <input type="hidden" id="quantity_hidden" name="quantity_hidden" class="controls" value=""/>
 <input type="hidden" id="price_hidden" name="price_hidden" class="controls" value=""/>
 <input type="hidden" id="total_hidden" name="total_hidden" class="controls" value=""/>
